254e97585af810270e3ec7dc38bc16be6a686f4a
Zafar Takhirov <z.tahirov@gmail.com>
2014-10-28 00:09:16 -0700

    Added AdMob

--- android/src/cc/rafazz/zombiebird/android/AndroidLauncher.java
+++ android/src/cc/rafazz/zombiebird/android/AndroidLauncher.java
@@ -1,16 +1,58 @@
 package cc.rafazz.zombiebird.android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import cc.rafazz.zombiebird.ZBGame;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import cc.rafazz.zombiebird.ZBGame;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new ZBGame(), config);
+		// initialize(new ZBGame(), config);
+
+		/*
+		 * AdMob:
+		 */
+		RelativeLayout layout = new RelativeLayout(this);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().clearFlags(
+				WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+		View gameView = initializeForView(new ZBGame(), config);
+
+		layout.addView(gameView);
+
+		// Create and setup the AdMob view
+		// AdView adView = new AdView(this, AdSize.BANNER, "xxxxxxxx"); // Put
+		// in your secret key here
+		AdView adView = new AdView(this);
+		adView.setAdSize(AdSize.SMART_BANNER);
+		adView.setAdUnitId(getResources().getString(R.string.adId));
+		// Gdx.app.log("Ads", getResources().getString(R.string.adId));
+		AdRequest adRequest = new AdRequest.Builder().build();
+		adView.loadAd(adRequest);
+
+		RelativeLayout.LayoutParams adParams = new RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.WRAP_CONTENT,
+				RelativeLayout.LayoutParams.WRAP_CONTENT);
+		adParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		adParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+		
+		layout.addView(adView, adParams);
+		adView.setVisibility(View.VISIBLE);
+		setContentView(layout);
 	}
 }
