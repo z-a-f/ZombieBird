284b752679050f6fe2fef71e7750d5dd42b6b227
Zafar Takhirov <z.tahirov@gmail.com>
2014-10-28 00:17:09 -0700

    Minor debug change

--- android/src/cc/rafazz/zombiebird/android/AndroidLauncher.java
+++ android/src/cc/rafazz/zombiebird/android/AndroidLauncher.java
@@ -7,7 +7,6 @@ import android.view.WindowManager;
 import android.widget.RelativeLayout;
 import cc.rafazz.zombiebird.ZBGame;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.google.android.gms.ads.AdRequest;
